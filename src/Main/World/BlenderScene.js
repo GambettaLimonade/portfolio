82414e50a5960e2824fbe657295fccd2cdb1de4a
Issam Merikhi <issammerikhi@outlook.fr>
2022-06-09 16:27:59 +0200

    remove fog

--- src/Main/World/BlenderScene.js
+++ src/Main/World/BlenderScene.js
@@ -23,7 +23,7 @@ export default class BlenderScene
         this.images = [this.resources.items.it1, this.resources.items.it2, this.resources.items.it3, this.resources.items.it4, this.resources.items.it5, this.resources.items.it6, this.resources.items.it7, this.resources.items.it8, this.resources.items.it9]
         this.imagesAnim = [this.resources.items.snk, this.resources.items.vsc]
         this.index = 0
-        this.fog = new THREE.Fog('#000000', 1, 300)
+        // this.fog = new THREE.Fog('#000000', 1, 300)
 
 
         this.petitScreenTv;
@@ -42,7 +42,7 @@ export default class BlenderScene
         this.model.rotation.y = Math.PI/2
         this.model.name = "Scene"
         this.scene.add(this.model)
-        this.scene.fog = this.fog
+        // this.scene.fog = this.fog
 
 
         
